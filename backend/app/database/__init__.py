a74ae75f5a12b9a10b16387e76e996ae31d090e9
Lohithaksh05 <lohitl57@gmail.com>
2025-06-13 19:49:39 +0530

    Fix database import to use correct MongoDB module based on environment

--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -1,3 +1,12 @@
-from .mongodb import MongoDB, connect_to_mongo, close_mongo_connection, get_database
+import os
+from decouple import config
 
-__all__ = ["MongoDB", "connect_to_mongo", "close_mongo_connection", "get_database"]
+# Import MongoDB connection based on environment
+is_render = os.getenv('RENDER') or config('PORT', default='8000') == '10000'
+
+if is_render:
+    from .mongodb_render import MongoDB, connect_to_mongo, close_mongo_connection, get_database, is_connected
+else:
+    from .mongodb import MongoDB, connect_to_mongo, close_mongo_connection, get_database, is_connected
+
+__all__ = ["MongoDB", "connect_to_mongo", "close_mongo_connection", "get_database", "is_connected"]
